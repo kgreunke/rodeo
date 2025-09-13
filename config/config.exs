24cc71d23685bdbcc8b2fccf9766c42659d2258f
Karl Greunke <kgreunke@gmail.com>
2025-09-13 16:58:38 -0500

    Gen auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :rodeo, :scopes,
+  user: [
+    default: true,
+    module: Rodeo.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Rodeo.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :rodeo,
   ecto_repos: [Rodeo.Repo],
   generators: [timestamp_type: :utc_datetime]
